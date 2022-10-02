100f83f3ed06e7ff843fa928322ecf47e00d48f5
Timothy Stephens <timothy.stephens1@uqconnect.edu.au>
2022-10-02 17:43:27 -0400

    Update pages on Sun Oct  2 17:43:27 EDT 2022

---
+++ render.r
@@ -0,0 +1,47 @@
+# This script builds both the HTML and PDF versions of your CV
+
+##
+## Full CV
+##
+# Knit the plain Markdown version
+rmarkdown::render("scripts/cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "../cv/cv.md")
+
+# Knit the HTML version
+rmarkdown::render("scripts/cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "../cv/cv.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("scripts/cv.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "../cv/cv.pdf")
+
+##
+## Short CV
+##
+# Knit the plain Markdown version
+rmarkdown::render("scripts/cv_short.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "../cv/cv.md")
+
+# Knit the HTML version
+rmarkdown::render("scripts/cv_short.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "../cv/cv_short.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("scripts/cv_short.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "../cv/cv_short.pdf")
