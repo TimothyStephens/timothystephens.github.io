18284c54bf7204cc3c65ad70042813ceaff17cc9
timothy.stephens1@uqconnect.edu.au <timothy.stephens1@uqconnect.edu.au>
2022-10-24 17:36:10 -0400

    Update pages on Mon Oct 24 17:36:10 EDT 2022

--- scripts/render_cv.r
+++ scripts/render_cv.r
@@ -3,16 +3,15 @@
 ##
 ## Full CV
 ##
-render_full_cv <- function(in.scripts="scripts", out.docs="../cv"){
-  print(paste(out.docs, "/cv.html", sep=''))
+render_full_cv <- function(){
   # Knit the HTML version
-  rmarkdown::render(paste(in.scripts, "/cv.rmd", sep=''),
+  rmarkdown::render("scripts/cv.rmd",
                     params = list(pdf_mode = FALSE),
-                    output_file = paste(out.docs, "/cv.html", sep=''))
+                    output_file = "../cv/cv.html")
   
   # Knit the PDF version to temporary html location
   tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-  rmarkdown::render(paste(in.scripts, "/cv.rmd", sep=''),
+  rmarkdown::render("scripts/cv.rmd",
                     params = list(pdf_mode = TRUE),
                     output_file = tmp_html_cv_loc)
   
@@ -24,19 +23,19 @@ render_full_cv <- function(in.scripts="scripts", out.docs="../cv"){
 ##
 ## Short CV
 ##
-render_short_cv <- function(in.scripts="scripts", out.docs="../cv"){
+render_short_cv <- function(){
   # Knit the HTML version
-  rmarkdown::render(paste(in.scripts, "/cv_short.rmd", sep=''),
+  rmarkdown::render("scripts/cv_short.rmd",
                     params = list(pdf_mode = FALSE),
-                    output_file = paste(out.docs, "/cv_short.html", sep=''))
+                    output_file = "../cv/cv_short.html")
   
   # Knit the PDF version to temporary html location
   tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-  rmarkdown::render(paste(in.scripts, "/cv_short.rmd", sep=''),
+  rmarkdown::render("scripts/cv_short.rmd",
                     params = list(pdf_mode = TRUE),
                     output_file = tmp_html_cv_loc)
   
   # Convert to PDF using Pagedown
   pagedown::chrome_print(input = tmp_html_cv_loc,
-                         output = paste(out.docs, "/cv_short.pdf", sep=''))
+                         output = "cv/cv_short.pdf")
 }
